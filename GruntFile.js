52f6b863ddec94ebfa6e02241da83e2b434a9e50
Pranav Shekhar Jha <prjha@ebay.com>
2016-02-11 18:46:07 +0530

    auditor formatting

--- GruntFile.js
+++ GruntFile.js
@@ -1,47 +1,185 @@
-/****************************************************************
- * Grunt File for a11y-auditor APP
- * Tasks performed :
- * 1. Run JSHint to lint the files.
- * 2. Run grunt-browserify task to convert the files to be front end compatible
- * 3. Have watchify to watch over the files changing under lib/*.js
- */
-
+'use strict';
+// # Grunt File for a11y-auditor APP
+//
+// Tasks performed :
+// 1. Run JSHint to lint the files.
+// 2. Run grunt-browserify task to convert the files to be front end compatible
+// 3. Have watchify to watch over the files changing under lib/*.js
+//
+// js-hint options. See the complete list of options [here](http://jshint.com/docs/options/)
+var jshintOptions = {
+    nonew: true,
+    plusplus: false,
+    curly: true,
+    latedef: true,
+    maxdepth: 6,
+    unused: true,
+    noarg: true,
+    trailing: true,
+    indent: 4,
+    forin: true,
+    noempty: true,
+    quotmark: true,
+    maxparams: 6,
+    node: true,
+    eqeqeq: true,
+    strict: true,
+    undef: true,
+    bitwise: true,
+    newcap: true,
+    immed: true,
+    camelcase: true,
+    maxcomplexity: 7,
+    maxlen: 120,
+    nonbsp: true,
+    freeze: true
+};
 module.exports = function(grunt) {
-
-    //load the grunt watcher
+    // loading the npm task
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-docco-plus');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-istanbul');
+    grunt.loadNpmTasks('grunt-coveralls');
+    grunt.loadNpmTasks('grunt-env');
     grunt.loadNpmTasks('grunt-contrib-watch');
-
-    //load the grunt browserify plugin
     grunt.loadNpmTasks('grunt-browserify');
 
-    //load the js hint plugin
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-
-
-    // configure tasks in grunt
+    // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        //jshint: {
-          //files: ['Gruntfile.js', 'lib/**/*.js']
-        //},
-        browserify: {
-            main: {
-                src: 'index.js',
-                dest: 'dist/browser/ally-auditor.js',
-                options : {
-                  browserifyOptions : {
-                    standalone : "index"
-                  }
+            pkg: grunt.file.readJSON('package.json'),
+            clean: [
+                '.coverage',
+                '.test',
+                '.cache'
+            ],
+            jshint: {
+                lib: {
+                    src: [
+                        'lib/**/*.js',
+                        'Gruntfile.js',
+                        'package.json'
+                    ],
+                    options: jshintOptions
                 }
+            },
+            // Configure a mochaTest task
+            mochaTest: {
+                test: {
+                    options: {
+                        reporter: 'spec',
+                        timeout: 50000
+                    },
+                    src: [
+                        'test/*.js'
+                    ]
+                }
+            },
+            instrument: {
+                files: [
+                    'lib/**/*.js'
+                ],
+                options: {
+                    lazy: false,
+                    basePath: '.coverage/instrument/'
+                }
+            },
+            storeCoverage: {
+                options: {
+                    dir: '.coverage/json/'
+                }
+            },
+            makeReport: {
+                src: '.coverage/json/*.json',
+                options: {
+                    type: 'lcov',
+                    dir: '.coverage/reports/',
+                    print: 'detail'
+                }
+            },
+            env: {
+                coverage: {
+                    APP_DIR_FOR_CODE_COVERAGE: '.coverage/instrument/'
+                }
+            },
+            'docco-plus': {
+                debug: {
+                    src: [
+                        'lib/**',
+                        'test/**',
+                        '*.js',
+                        '*.md'
+                    ],
+                    options: {
+                        output: '.docs/'
+                    }
+                }
+            },
+            'gh-pages': {
+                options: {
+                    base: '.docs',
+                    // GH_TOKEN is the environment variable holding the access token for the repository
+                    repo: 'https://' + process.env.GH_TOKEN + '@github.com/' + process.env.TRAVIS_REPO_SLUG + '.git',
+                    clone: '.gh_pages',
+                    message: 'build #' + process.env.TRAVIS_BUILD_NUMBER + ' travis commit',
+                    // This configuration will suppress logging and sanitize error messages.
+                    silent: true,
+                    user: {
+                        name: 'travis',
+                        email: 'travis@travis-ci.com'
+                    }
+                },
+                src: [
+                    '**'
+                ]
+            },
+            coveralls: {
+                lcov: {
+                    // LCOV coverage file relevant to every target
+                    src: '.coverage/reports/lcov.info'
+                }
+            },
+            browserify: {
+                main: {
+                    src: 'index.js',
+                    dest: 'dist/browser/ally-auditor.js',
+                    options: {
+                        browserifyOptions: {
+                            standalone: "index"
+                        }
+                    }
+                }
+            },
+            watch: {
+                files: [
+                    'lib/**/*.js',
+                    'index.js'
+                ],
+                tasks: ['default']
             }
-        },
-        watch: {
-            files: ['lib/**/*.js', 'index.js'],
-            tasks: ['default']
         }
-    });
-
+    );
+    grunt.registerTask('test', [
+        'jshint',
+        'mochaTest'
+    ]);
+    grunt.registerTask('coverage', [
+        'instrument',
+        'env:coverage',
+        'mochaTest',
+        'storeCoverage',
+        'makeReport',
+        'coveralls:lcov'
+    ]);
+    grunt.registerTask('document', [
+        'docco-plus'
+    ]);
     //register the grunt tasks that need to be executed
-    grunt.registerTask('default', [/*'jshint', */'browserify', 'watch']);
-
+    grunt.registerTask('default', [
+        /*'jshint', */
+        'browserify',
+        'watch'
+    ]);
 };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //defining a global root dirname to save the trouble from relative paths
 //global.__base = __dirname + '/';
 
-//load the audit runner and export it
+// load the audit runner and export it
 var auditRunner = require("./lib/audit/auditRunner");
 
 module.exports = auditRunner;
