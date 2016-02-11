3a924e4d349eb48f36980e52b2955bce3f131712
Damodaran Sathyakumar <dasathyakuma@ebay.com>
2016-02-11 15:37:47 +0530

    commenting out the global directory defn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //defining a global root dirname to save the trouble from relative paths
-global.__base = __dirname + '/';
+//global.__base = __dirname + '/';
 
 //load the audit runner and export it
 var auditRunner = require("./lib/audit/auditRunner");
